d0dd48c1fc74654638a3a1be2061038576fc6b6c
Alexander Lebedev <alexander.s.lebedev@gmail.com>
2019-11-23 15:17:47 +0700

    Initial Commit

---
+++ Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by alexander on 23.11.2019.
+//  Copyright © 2019 alexander. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
